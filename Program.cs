8b78d78b996f38bf4e3c896f54183ecdf238e5d4
Stefan Henneken <stefan.henneken@live.com>
2017-06-07 21:59:44 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Threading;
+
+namespace Sample03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().Run();
+        }
+        public void Run()
+        {
+            try
+            {
+                foreach (int result in Work(10))
+                    Console.WriteLine("result: " + result);
+            }
+            catch (AggregateException aex)
+            {
+                Console.WriteLine("AggregateException in Run: " + aex.Message);
+                aex.Flatten();
+                foreach (Exception ex in aex.InnerExceptions)
+                    Console.WriteLine("  Exception in Run: " + ex.Message);
+            }
+            Console.ReadLine();
+        }
+        private IEnumerable<int> Work(int a)
+        {
+            int[] values = new int[] { 1, 0, 5, 0, 2 };
+            List<Exception> list = new List<Exception>();
+            int r;
+            foreach (int value in values)
+            {
+                try
+                {
+                    r = a / value;
+                }
+                catch (Exception ex)
+                {
+                    r = -1;
+                    list.Add(ex);
+                }
+                yield return r;
+            }
+            if (list.Count > 0)
+                throw new AggregateException(list);
+        }
+    }
+}
